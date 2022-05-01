55cad65f43208cb953e7a943f9a67d9641270e62
Jan Wawszczak <janekw23@gmail.com>
2022-05-01 15:06:24 +0200

    Add loading edges from file

--- brandes.cu
+++ brandes.cu
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main() {
-    std::cout << "Hello world\n";
+using namespace std;
+
+static void print_usage() {
+    cout << "Program usage: ./brandes input-file output-file" << endl;
+}
+
+static void check_args(int argc, char *argv[]) {
+    if (argc != 3 || !strcmp(argv[1], "") || !strcmp(argv[2], "")) {
+        print_usage();
+        exit(1);
+    }
+}
+
+static vector<pair<int, int>> load_edges(string path) {
+    ifstream ifs;
+    ifs.open(path);
+    if(!ifs.is_open()) {
+        cerr << "Cannot open file \"" << path << "\"." << endl;
+        exit(1);
+    }
+
+    vector<pair<int, int>> edges;
+
+    while(ifs.good()) {
+        int u, v;
+        ifs >> u >> v;
+        edges.emplace_back(u, v);
+    }
+
+    ifs.close();
+    return edges;
+} 
+
+int main(int argc, char *argv[]) {
+    check_args(argc, argv);
+    auto edges = load_edges(argv[1]);
+
+    for (auto edge : edges) {
+        cout << edge.first << "-" << edge.second << "\n";
+    }
 
     return 0;
 }
